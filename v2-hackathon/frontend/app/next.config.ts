fe448d27ea44e041a4b8f0530a060f498ed9666b
Arpit Mishra <arpit.mishra@lgads.tv>
2026-06-05 01:48:10 +0530

    f

--- v2-hackathon/frontend/app/next.config.ts
+++ v2-hackathon/frontend/app/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["10.4.0.215", "10.4.0.241"],
+  allowedDevOrigins: ["10.4.0.215", "10.4.0.241", "10.4.0.177"],
 };
 
 export default nextConfig;
