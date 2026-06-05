19dd5cbf006176ea417b0b793a7c96e8bffb2217
Pratikesh Thakur <pratikeshthakur@pratikesh.thakur>
2026-06-05 07:50:21 +0530

    removeing backup front end

--- v2-hackathon/frontend-backup/app/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  allowedDevOrigins: ["10.4.0.215", "10.4.0.241", "10.4.0.177"],
-  devIndicators: false,
-  reactStrictMode: false,
-};
-
-export default nextConfig;
